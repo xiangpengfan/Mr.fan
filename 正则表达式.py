65133c037b68d0d2e8e0e8e5fb01d96defc312b6
xiangpengfan <2524994091@qq.com>
2026-05-05 15:06:39 +0800

    Add files via upload

--- 正则表达式.py
+++ 正则表达式.py
@@ -14,9 +14,11 @@ def is_valid_email(addr):
         return False
 
 def name_of_email(addr):
-    pattern=r'^([a-zA-Z0-9]+)@[a-zA-z0-9]+\.[a-zA-Z]{3}$'
-    pattern2=r'^[<]([a-zA-Z0-9 ]+)[>][a-zA-Z ]+@[a-zA-z0-9]+\.[a-zA-Z]{3}$'
-    if re.match(pattern2, addr):
+    # pattern=r'^([a-zA-Z0-9]+)@[a-zA-z0-9]+\.[a-zA-Z]{3}$'
+    # pattern2=r'^[<]([a-zA-Z0-9 ]+)[>][a-zA-Z ]+@[a-zA-z0-9]+\.[a-zA-Z]{3}$'
+    pattern=r'^([a-zA-Z0-9]+)@'
+    pattern2=r'^[<]([a-zA-Z0-9 ]+)[>][a-zA-Z ]+@'   #这是优化过后的表达式，只需要输出名字，不需要判断邮箱地址了
+    if re.match(pattern2, addr):cc
         return re.match(pattern2, addr).group(1)
     else:
         return re.match(pattern, addr).group(1)     #把不同的邮箱地址的用户名按照不同的正则表达式提取出来，返回用户名
